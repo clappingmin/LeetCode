912bc8f7395ddcdd780e64791247adb5e5f61246
Sumin Park <clappingmin@gmail.com>
2024-05-22 14:38:07 +0900

    Time: 49 ms (74.57%), Space: 48.3 MB (93.79%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -4,14 +4,13 @@
  */
 var plusOne = function(digits) {
     const len = digits.length;
-    let carry = false;
+    let carry = false; // 올림수 처리
     
-    for (let i = len -1; i>=0; i--) {
+    for (let i = len-1; i>=0; i--) {
         
-        if (carry) carry = false;
-    
         const plusNum = digits[i] + 1;
         digits.splice(i, 1, plusNum % 10);
+        carry = false;
         
         if (plusNum < 10) return digits;
         else carry = true;
